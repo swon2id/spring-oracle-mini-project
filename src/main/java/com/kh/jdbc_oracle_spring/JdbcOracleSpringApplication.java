a7021dd0004c6c2ea7c57ecf569b6f67aba348f8
Lee Sangwon <83111797+swon2id@users.noreply.github.com>
2024-10-23 16:41:17 +0900

    first commit

---
+++ src/main/java/com/kh/jdbc_oracle_spring/JdbcOracleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kh.jdbc_oracle_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcOracleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcOracleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/jdbc_oracle_spring/JdbcOracleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.jdbc_oracle_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcOracleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
